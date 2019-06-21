46bf59bd181736c24b211fbbdcaf2f2daed052f6
Larkin Nickle <me@larbob.org>
2019-06-21 16:43:10 -0400

    Update bash to 5.0

--- bash/package.sh
+++ bash/package.sh
@@ -1,6 +1,6 @@
 #!/opt/local/bin/mksh ../.port.sh
 port=bash
-version=4.4
+version=5.0
 useconfigure=true
-files="http://ftp.gnu.org/gnu/bash/bash-4.4.tar.gz bash-4.4.tar.gz 8de012df1e4f3e91f571c3eb8ec45b43d7c747eb"
+files="https://ftp.gnu.org/gnu/bash/bash-5.0.tar.gz bash-5.0.tar.gz d116b469b9e6ea5264a74661d3a4c797da7f997b"
 patchlevel=0
