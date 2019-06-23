df827434a054f6f5017214ddceeda4fe496a9955
Larkin Nickle <me@larbob.org>
2019-06-23 15:23:52 -0400

    Add sed port

---
+++ sed/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=sed
+version=4.7
+useconfigure=true
+files="http://ftp.gnu.org/gnu/sed/sed-4.7.tar.xz sed-4.7.tar.xz dbc842f6fdf538f366d97e7c4b4f241eaefd14a5"
