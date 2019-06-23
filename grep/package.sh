6a4ef1aa8b04f5f7f5755c97bccb04967a95d00c
Larkin Nickle <me@larbob.org>
2019-06-23 15:25:53 -0400

    Add grep port

---
+++ grep/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=grep
+version=3.3
+useconfigure=true
+files="http://ftp.gnu.org/gnu/grep/grep-3.3.tar.xz grep-3.3.tar.xz e0befe21e7d9caa8e5e98385c96355d890f83123"
