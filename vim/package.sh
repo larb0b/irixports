4e8fda8264c431af55b5a97828cbf51939b802e0
Larkin Nickle <me@larbob.org>
2019-07-06 22:36:42 -0300

    Update vim to 8.1.1640

--- vim/package.sh
+++ vim/package.sh
@@ -1,7 +1,7 @@
 #!/opt/local/bin/mksh ../.port.sh
 port=vim
-version=8.1
-workdir=vim81
+version=8.1.1640
 useconfigure=true
-files="http://ftp.vim.org/pub/vim/unix/vim-8.1.tar.bz2 vim-8.1.tar.bz2 cbca219d11990d866976da309d7ce5b76be48b96"
+curlopts="-L"
+files="https://github.com/vim/vim/archive/v8.1.1640.tar.gz vim-8.1.1640.tar.gz aa8c11175b19ce6735b0a80bf6ba1f817a644ca5"
 configopts="--with-features=huge"
