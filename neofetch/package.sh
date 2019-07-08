cbfc0b35b25c3a4bc88ade14002de56609c30b1e
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-08 18:37:11 -0400

    initial

---
+++ neofetch/package.sh
@@ -0,0 +1,6 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=neofetch
+version=6.0.0
+workdir=neofetch-6.0.0
+useconfigure=true
+files="https://github.com/dylanaraps/neofetch/archive/6.0.0.tar.gz 6.0.0.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
