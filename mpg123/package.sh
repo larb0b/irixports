98c8475cd1800d26a0638d2dddebda0e32cec50e
Eric Dodd <eric.dodd@etrade.com>
2019-07-09 19:30:01 -0400

    initial

---
+++ mpg123/package.sh
@@ -0,0 +1,4 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=mpg123
+version=1.25.10
+files="https://versaweb.dl.sourceforge.net/project/mpg123/mpg123/1.25.10/mpg123-1.25.10.tar.bz2 mpg123-1.25.10.tar.bz2 e6d119755acdf9104d7ba236b1242696940ed6dd"
