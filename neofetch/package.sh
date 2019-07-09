27b70b1824ce3e4b39508611b5f0b09e84c765d4
Eric Dodd <eric.dodd@etrade.com>
2019-07-09 19:19:04 -0400

    Refactored neofetch

--- neofetch/package.sh
+++ neofetch/package.sh
@@ -1,8 +1,6 @@
 #!/opt/local/bin/mksh ../.port.sh
 port=neofetch
 version=6.0.0
-workdir=neofetch-6.0.0
-useconfigure=false
-files="https://codeload.github.com/dylanaraps/neofetch/tar.gz/6.0.0 6.0.0.tar.gz d78dcf1d6352eafa616f9d484e36cd03804a0d6c"
-
-export PREFIX=$prefix
+curlopts="-L"
+files="https://github.com/dylanaraps/neofetch/archive/6.0.0.tar.gz neofetch-6.0.0.tar.gz d78dcf1d6352eafa616f9d484e36cd03804a0d6c"
+depends="bash"
