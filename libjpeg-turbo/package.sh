d4a13d95c1f277f505925a5e055d3a684be46663
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-20 11:15:40 -0400

    porting libjpeg-turbo

---
+++ libjpeg-turbo/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=libjpeg-turbo
+version=2.0.2
+useconfigure=true
+files="https://newcontinuum.dl.sourceforge.net/project/libjpeg-turbo/2.0.2/libjpeg-turbo-2.0.2.tar.gz libjpeg-turbo-2.0.2.tar.gz 0bdcf9055eb22157f39a1695"
