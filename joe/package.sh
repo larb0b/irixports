b85b7d965c508ae3882d36b30b6930103049b4c5
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-20 16:08:08 -0400

    porting joe

--- joe/package.sh
+++ joe/package.sh
@@ -2,4 +2,4 @@
 port=joe
 version=4.6
 useconfigure=true
-files="https://ayera.dl.sourceforge.net/project/joe-editor/JOE%20sources/joe-4.6/joe-4.6.tar.gz joe-4.6.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
+files="https://ayera.dl.sourceforge.net/project/joe-editor/JOE%20sources/joe-4.6/joe-4.6.tar.gz joe-4.6.tar.gz da7712333f676cfb5ae9f3565fd2225c5a2185ca"
