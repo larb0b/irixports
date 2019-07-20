382438fabc9a279b6e4b2db938e029a704369924
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-20 16:03:13 -0400

    initial

---
+++ joe/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=joe
+version=4.6
+useconfigure=true
+files="https://ayera.dl.sourceforge.net/project/joe-editor/JOE%20sources/joe-4.6/joe-4.6.tar.gz joe-4.6.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
