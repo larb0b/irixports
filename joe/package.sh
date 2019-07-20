712a47e7b29573a851db1629a3aed461153f4093
unxmaal <eric.e.dodd@gmail.com>
2019-07-20 17:12:17 -0400

    Add joe port

---
+++ joe/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=joe
+version=4.6
+useconfigure=true
+files="https://ayera.dl.sourceforge.net/project/joe-editor/JOE%20sources/joe-4.6/joe-4.6.tar.gz joe-4.6.tar.gz da7712333f676cfb5ae9f3565fd2225c5a2185ca"
