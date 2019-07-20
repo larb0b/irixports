76841973818f0e65de3b9a5b8a26b56efbe15b8d
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-20 11:17:12 -0400

    sha1

--- libjpeg-turbo/package.sh
+++ libjpeg-turbo/package.sh
@@ -2,4 +2,4 @@
 port=libjpeg-turbo
 version=2.0.2
 useconfigure=true
-files="https://newcontinuum.dl.sourceforge.net/project/libjpeg-turbo/2.0.2/libjpeg-turbo-2.0.2.tar.gz libjpeg-turbo-2.0.2.tar.gz 0bdcf9055eb22157f39a1695"
+files="https://newcontinuum.dl.sourceforge.net/project/libjpeg-turbo/2.0.2/libjpeg-turbo-2.0.2.tar.gz libjpeg-turbo-2.0.2.tar.gz 1cff52d50b81755d0bdcf9055eb22157f39a1695"
