7730c0a03fb5866f56b16df96c7be1afd5e566f7
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-21 16:06:19 -0400

    added git

---
+++ git/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=git
+version=2.20.1
+useconfigure=true
+files="https://codeload.github.com/git/git/tar.gz/v2.20.1 git-2.20.1  e6d119755acdf9104d7ba236b1242696940ed6dd"
