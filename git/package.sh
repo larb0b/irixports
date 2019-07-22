4b21917eae20564aaa938f23e9aecdb3fc56ba0f
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-21 20:32:31 -0400

    not yet working

--- git/package.sh
+++ git/package.sh
@@ -2,4 +2,13 @@
 port=git
 version=2.20.1
 useconfigure=true
-files="https://codeload.github.com/git/git/tar.gz/v2.20.1 git-2.20.1  e6d119755acdf9104d7ba236b1242696940ed6dd"
+files="https://codeload.github.com/git/git/tar.gz/v2.20.1 git-2.20.1.tar.gz 7813d793e2f5ba5efb62a253b288b43d750c6471"
+configopts="--with-openssl=/opt/local/openssl"
+
+configure(){
+	runwd aclocal
+	runwd autoconf
+	#runwd automake
+	runwd ./configure --prefix="$prefix" $configopts
+}
+
