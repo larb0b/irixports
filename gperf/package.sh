409b261b2281be45e9adae7729a45b337a808879
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-23 17:51:45 -0400

    switched to gcc 4.7.4

--- gperf/package.sh
+++ gperf/package.sh
@@ -1,5 +1,6 @@
 #!/opt/local/bin/mksh ../.port.sh
+gccversion=4.7.4
 port=gperf
 version=3.1
 useconfigure=true
-files="http://ftp.gnu.org/pub/gnu/gperf/gperf-3.1.tar.gz gperf-3.1.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
+files="http://ftp.gnu.org/pub/gnu/gperf/gperf-3.1.tar.gz gperf-3.1.tar.gz e3c0618c2d2e5586eda9498c867d5e4858a3b0e2"
