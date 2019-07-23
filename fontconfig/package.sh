4ff34b67fc886eedce9b13cbea9f54246de5c10f
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-22 22:16:15 -0400

    working on fc

--- fontconfig/package.sh
+++ fontconfig/package.sh
@@ -2,4 +2,13 @@
 port=fontconfig
 version=2.13.1
 useconfigure=true
-files="://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.1.tar.gz fontconfig-2.13.1.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
+files="https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.1.tar.gz fontconfig-2.13.1.tar.gz e073e1d23d9d6e83a8d2d6eafa5905a541b77975"
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/people/edodd/.local/lib/pkgconfig
+
+configure(){
+	runwd aclocal
+	runwd autoconf
+	runwd automake
+	runwd ./configure --prefix="$prefix" $configopts
+}
