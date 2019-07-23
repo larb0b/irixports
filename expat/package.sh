1dbb21728518e650be4b55ce3fc5af44e9714d3f
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-22 21:53:57 -0400

    added expat

--- expat/package.sh
+++ expat/package.sh
@@ -1,5 +1,11 @@
 #!/opt/local/bin/mksh ../.port.sh
 port=expat
 version=2.2.7
+workdir=libexpat-R_2_2_7/expat
 useconfigure=true
-files="https://codeload.github.com/libexpat/libexpat/tar.gz/R_2_2_7 libexpat-2.2.7.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
+files="https://codeload.github.com/libexpat/libexpat/tar.gz/R_2_2_7 libexpat-2.2.7.tar.gz fedb0c4c822374fc4e03fbd1ca996f3ac8aa2781"
+
+configure(){
+	runwd ./buildconf.sh
+	runwd ./configure --prefix="$prefix" $configopts
+}
