b034f60d216bd71d8d671cd909d606e18cfdaf36
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-22 21:39:15 -0400

    added expat

---
+++ expat/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=expat
+version=2.2.7
+useconfigure=true
+files="https://codeload.github.com/libexpat/libexpat/tar.gz/R_2_2_7 libexpat-2.2.7.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
