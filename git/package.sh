7e19f96795f9f2529547d986eda3744a81674d8b
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-22 20:01:08 -0400

    ported git

--- git/package.sh
+++ git/package.sh
@@ -3,7 +3,7 @@ port=git
 version=2.20.1
 useconfigure=true
 files="https://codeload.github.com/git/git/tar.gz/v2.20.1 git-2.20.1.tar.gz 7813d793e2f5ba5efb62a253b288b43d750c6471"
-configopts="--with-openssl=/opt/local/openssl"
+configopts="--with-openssl=/opt/local/openssl --with-tcltk"
 
 configure(){
 	runwd aclocal
