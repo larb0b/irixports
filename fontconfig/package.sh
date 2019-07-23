e2c6b9533f21d25bbdc40c35e2f84c1cf961018f
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-22 21:36:41 -0400

    ported fontconfig

---
+++ fontconfig/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=fontconfig
+version=2.13.1
+useconfigure=true
+files="://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.1.tar.gz fontconfig-2.13.1.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
