080488542c9d9ef9941f852417e2710d919a4f07
Eric Dodd <eric.e.dodd@gmail.com>
2019-07-22 22:17:51 -0400

    added gperf

---
+++ gperf/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=gperf
+version=3.1
+useconfigure=true
+files="http://ftp.gnu.org/pub/gnu/gperf/gperf-3.1.tar.gz gperf-3.1.tar.gz e6d119755acdf9104d7ba236b1242696940ed6dd"
