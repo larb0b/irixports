7dcc6885d58867dd362a9360c2bb4b600b94aaa9
John Sennesael <john@adminking.com>
2019-08-23 10:53:28 -0500

    Add autoconf port

---
+++ autoconf/package.sh
@@ -0,0 +1,6 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=autoconf
+version=2.69
+useconfigure=true
+files="http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz autoconf-2.69.tar.gz 562471cbcb0dd0fa42a76665acf0dbb68479b78a"
+depends=m4
