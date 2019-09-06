0d6211b58af530317cbaa7c017520f8b120d264d
Eric Dodd <eric.e.dodd@gmail.com>
2019-09-06 16:40:32 -0700

    added sudo

---
+++ sudo/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../.port.sh
+port=sudo
+version=1.8.24
+useconfigure=true
+files="ftp://ftp.sudo.ws/pub/sudo/sudo-1.8.24.tar.gz sudo-1.8.24.tar.gz 7f2c1dd556223a310a4c7d897176b8d3ff3a6089"
