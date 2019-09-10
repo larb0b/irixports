c93a6a997fe50078ec9f643af91670ec5ec065af
Larkin Nickle <me@larbob.org>
2019-09-10 18:46:44 -0400

    Add GNU coreutils port

---
+++ ports/coreutils/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=coreutils
+version=8.31
+useconfigure=true
+files="https://ftp.gnu.org/gnu/coreutils/coreutils-8.31.tar.xz coreutils-8.31.tar.xz c62b5034a5b65daca33c50a60df87ebc44159faf"
