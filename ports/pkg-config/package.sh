ff6c2f69f3241533a471e90508c7b4ae93e97442
Larkin Nickle <me@larbob.org>
2019-09-11 21:47:29 -0400

    Move pkg-config to proper dir

--- ports/pkg-config/package.sh
+++ ports/pkg-config/package.sh
@@ -4,4 +4,3 @@ version=0.29.2
 useconfigure=true
 configopts="--with-internal-glib"
 files="https://pkg-config.freedesktop.org/releases/pkg-config-0.29.2.tar.gz pkg-config-0.29.2.tar.gz 76e501663b29cb7580245720edfb6106164fad2b"
-
