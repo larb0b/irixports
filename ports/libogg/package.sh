ce2ba14ddbb0ba57f1dae31f0b37626fa4d3b94a
unxmaal <eric.e.dodd@gmail.com>
2019-09-20 23:54:38 -0400

    Add libogg port

---
+++ ports/libogg/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=libogg
+version=1.3.4
+useconfigure=true
+files="https://ftp.osuosl.org/pub/xiph/releases/ogg/libogg-1.3.4.tar.gz libogg-1.3.4.tar.gz 851cef020b346d44893e5d1c3dab83c675d479d9"
