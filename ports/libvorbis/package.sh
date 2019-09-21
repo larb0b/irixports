a6e10e08a6aba4af6aed63fe239002fddda8a510
unxmaal <eric.e.dodd@gmail.com>
2019-09-20 23:54:49 -0400

    Add libvorbis port

---
+++ ports/libvorbis/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=libvorbis
+version=1.3.6
+useconfigure=true
+files="https://ftp.osuosl.org/pub/xiph/releases/vorbis/libvorbis-1.3.6.tar.gz libvorbis-1.3.6.tar.gz 91f140c220d1fe3376d637dc5f3d046263784b1f"
