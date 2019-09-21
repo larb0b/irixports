4257627f0b3ba730bc40415f60214096e357962d
Eric Dodd <eric.e.dodd@gmail.com>
2019-09-20 21:06:02 -0400

    added libao

---
+++ ports/libao/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=libao
+version=1.2.0
+useconfigure=true
+files="https://ftp.osuosl.org/pub/xiph/releases/ao/libao-1.2.0.tar.gz libao-1.2.0.tar.gz 6b1d2c6a2e388e3bb6ebea158d51afef18aacc56"
