6e5b9482d0cd47df663fe7c28ae9193b9e49b46a
Eric Dodd <eric.e.dodd@gmail.com>
2019-09-20 20:58:21 -0400

    added flac

---
+++ ports/libflac/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=flac
+version=1.3.2
+useconfigure=true
+files="https://ftp.osuosl.org/pub/xiph/releases/flac/flac-1.3.2.tar.xz flac-1.3.2.tar.xz 2bdbb56b128a780a5d998e230f2f4f6eb98f33ee"
