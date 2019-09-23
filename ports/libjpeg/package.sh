6b537ef792bc9ebe56dec4213780af6870f875db
Eric Dodd <eric.e.dodd@gmail.com>
2019-09-23 18:31:15 -0400

    added libjpeg

---
+++ ports/libjpeg/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=jpeg
+version=9c
+useconfigure=true
+files="http://www.ijg.org/files/jpegsrc.v9c.tar.gz jpegsrc.v9c.tar.gz  2ce111c8c0ac828a44b13ad28c265e954a342d07"
