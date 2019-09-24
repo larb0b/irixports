0161f568de7eea3adf171a4a1bdc70c904b650a3
Larkin <45925460+larb0b@users.noreply.github.com>
2019-09-23 21:24:23 -0400

    Update package.sh

--- ports/libjpeg/package.sh
+++ ports/libjpeg/package.sh
@@ -2,4 +2,4 @@
 port=jpeg
 version=9c
 useconfigure=true
-files="http://www.ijg.org/files/jpegsrc.v9c.tar.gz jpegsrc.v9c.tar.gz  2ce111c8c0ac828a44b13ad28c265e954a342d07"
+files="http://www.ijg.org/files/jpegsrc.v9c.tar.gz jpegsrc.v9c.tar.gz 2ce111c8c0ac828a44b13ad28c265e954a342d07"
