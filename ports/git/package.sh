60d49171549174cb6fc1e479df637ef33f52a4a6
Larkin Nickle <me@larbob.org>
2019-09-26 20:28:05 -0400

    git: Use irixports OpenSSL, use _rld_new_interface hack

--- ports/git/package.sh
+++ ports/git/package.sh
@@ -3,11 +3,11 @@ port=git
 version=2.20.1
 useconfigure=true
 files="https://codeload.github.com/git/git/tar.gz/v2.20.1 git-2.20.1.tar.gz 7813d793e2f5ba5efb62a253b288b43d750c6471"
-configopts="--with-openssl=/opt/local/openssl --with-tcltk"
+configopts="--with-openssl=$prefix --with-tcltk CFLAGS=-Wl,--unresolved-symbols=ignore-all"
+depends="openssl"
 
 configure(){
 	runwd aclocal
 	runwd autoconf
-	#runwd automake
 	runwd ./configure --prefix="$prefix" $configopts
 }
