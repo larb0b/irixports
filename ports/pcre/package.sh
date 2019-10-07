263b4cc19bfd1f0d359bd30275979b76fb2498f9
Andrew Diller <dillera@gmail.com>
2019-10-06 23:38:11 -0700

    added pcre-8.43

---
+++ ports/pcre/package.sh
@@ -0,0 +1,6 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=pcre
+buildtools=dibs-.4-9
+version=8.43
+useconfigure=true
+files="https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.bz2 pcre-8.43.tar 0d4585ee6426ab0db9c0c8f1c8c6da968170174d"
