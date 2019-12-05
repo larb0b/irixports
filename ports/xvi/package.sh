ed25d9b6a45c8b2dda782350454bc6fc30923b14
Larkin Nickle <me@larbob.org>
2019-12-05 12:58:59 -0500

    Add xvi port

---
+++ ports/xvi/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=xvi
+version=2.50.3
+files="http://martinwguy.github.io/xvi/download/xvi-2.50.3.tar.gz xvi-2.50.3.tar.gz 0b53dff9dab8ad62dea5aae6d8e1372f7218f3dd"
+depends="termcap"
