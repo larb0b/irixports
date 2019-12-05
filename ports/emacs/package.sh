3c1eac1c8f0be5a8b51dc91d7b6a8def61fb9a9a
Larkin Nickle <me@larbob.org>
2019-12-05 16:17:00 -0500

    emacs/package.sh: Remove gccversion variable

--- ports/emacs/package.sh
+++ ports/emacs/package.sh
@@ -5,5 +5,4 @@ version=24.5.1
 workdir=emacs-24.5
 useconfigure=true
 configopts="--with-xpm=no --with-jpeg=no --with-gif=no --with-tiff=no"
-gccversion=4.7.4
 files="ftp://ftp.gnu.org/gnu/emacs/emacs-24.5.tar.gz emacs-24.5.1.tar.gz 806e80eefa2ca89d2f8c0917f8b61c5c87af4f2d"
