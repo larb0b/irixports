d4e2075d65f7b874a634341d1412479a29eb089c
Larkin Nickle <me@larbob.org>
2019-12-05 12:58:49 -0500

    Add termcap port

---
+++ ports/termcap/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=termcap
+version=1.3.1
+useconfigure=true
+files="http://ftp.gnu.org/gnu/termcap/termcap-1.3.1.tar.gz termcap-1.3.1.tar.gz 42dd1e6beee04f336c884f96314f0c96cc2578be"
