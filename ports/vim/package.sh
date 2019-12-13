8e02586e5d7ce7cb696087c7d49300ebcbcc0ecc
Larkin Nickle <me@larbob.org>
2019-12-13 15:21:17 -0500

    Update vim to 8.2

--- ports/vim/package.sh
+++ ports/vim/package.sh
@@ -1,7 +1,7 @@
 #!/opt/local/bin/mksh ../../.port.sh
 port=vim
-version=8.1.1662
+version=8.2
+workdir=vim82
 useconfigure=true
-curlopts="-L"
-files="https://github.com/vim/vim/archive/v8.1.1662.tar.gz vim-8.1.1662.tar.gz faffd88f4be9af5d81be8abe26f0425dee625e5c"
+files="https://ftp.nluug.nl/pub/vim/unix/vim-8.2.tar.bz2 vim-8.2.tar.bz2 463c71ce8289923810ee594751a9f38200ad82e2"
 configopts="--with-features=huge"
