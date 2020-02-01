54edbcb150648a2785bf63c51ec0e555a1fa15cf
Larkin Nickle <me@larbob.org>
2020-02-01 01:55:15 -0500

    Add pkgconf port
    Thanks Raion!

---
+++ ports/pkgconf/package.sh
@@ -0,0 +1,5 @@
+#!/opt/local/bin/mksh ../../.port.sh
+port=pkgconf
+version=0.9.6
+useconfigure=true
+files="https://files.etoilebsd.net/pkgconf/pkgconf-0.9.6.tar.bz2 pkgconf-0.9.6.tar.bz2 4514cd203c96482caef65ddd51f753e4b754f468"
